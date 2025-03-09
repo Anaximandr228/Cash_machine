cb2879566130e5a9ef61ae923918726e5adf390d
Alex <alex09.g29@gmail.com>
2025-03-09 23:19:08 +0500

    Edit database.py

--- Cash_machine/database.py
+++ Cash_machine/database.py
@@ -1,9 +1,18 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cash_machine.settings')
+
+import django
+django.setup()
+
 from machine.models import Item
-def ctreate_items():
+def create_items():
     items_list = Item.objects.bulk_create([
         Item(title="Choolate", price=135),
         Item(title="Icecream", price=150),
         Item(title="Сookie", price=25),
         Item(title="Сupcake", price=120),
 
-])
+    ])
+
+
+create_items()
